4a607b38d17a09d22af79d7e8e5331d69297647f
daniel shlaim <dada373@gmail.com>
2022-05-25 23:48:39 +0300

    Add project files.

---
+++ Basic Web Api HW/Controllers/FlowerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basic_Web_Api_HW.Controllers
+{
+    [ApiController]
+    [Route("flowercontroller/[action]")]
+    public class FlowerController
+    {
+
+        public string Flower1()
+        {
+            return "Rose";
+        }
+        public string Flower2()
+        {
+            return "Anemone";
+        }
+        public string Flower3()
+        {
+            return "Cyclamen";
+        }
+        public string Flower4()
+        {
+            return "Narcissus";
+        }
+    }
+}
